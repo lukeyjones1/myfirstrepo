cb092d5c01732c53c600ec164e47cf7ab89d66c3
lukeyjones1 <58837261+lukeyjones1@users.noreply.github.com>
2019-12-13 04:05:16 +0000

    Add files via upload
    Uploaded an Index.php file onto my first repo.

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Luke's First Website</title>
+    <link rel="css/styles.css">
+</head>
+
+<body>
+    <!-- This is just a test website file to be uploaded to GitHub -->
+        <!-- Header -->
+    <h1>Welcome to my first website!</h1>
+        <!-- Paragraph -->
+    <p>This is a very rough and brief example of a website so I can try uploading this to GIT and get a feel for working with github.</p>
+</body>
+</html>
